0cfbbad9055279effe5fa5b4705028334f81efa8
Andre Kradolfer <narfdre@gmail.com>
2012-09-05 22:07:42 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omakase::Application.config.secret_token = '5480c6b60609428caed618ea6f92997a2f5f2f677244fc90335da448704b17f26851efa694db7d73eb65de0049b1a2c54745de0f1aa6aaa68e43b3d69d30d67f'
