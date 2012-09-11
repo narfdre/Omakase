70c9196acf15e4af675773cbb30e615ec1c5202e
Andre Kradolfer <narfdre@gmail.com>
2012-09-11 15:19:32 -0600

    making images more responsive

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,11 @@ CarrierWave.configure do |config|
   config.storage              = :grid_fs
   config.grid_fs_access_url   = "/images"
   config.grid_fs_database     = Mongoid.database.name
+  config.grid_fs_host         = Mongoid.database.connection.primary_pool.host
+  config.grid_fs_port         = Mongoid.database.connection.primary_pool.port
+  config.grid_fs_username     = Mongoid.database.connection.auths[0]['username']
+  config.grid_fs_password     = Mongoid.database.connection.auths[0]['password']
+
 
   if Rails.env.production?
     config.grid_fs_database   = ENV['MONGOID_DATABASE'] || Mongoid.database.name
