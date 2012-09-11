891c645ebe837daa4c397fc29fa7499c3a5db7ef
Andre Kradolfer <narfdre@gmail.com>
2012-09-10 23:50:29 -0600

    adding products to home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+
+$(document).ready(function(){
+	$('.nav li a').on('click', setActiveNav);
+
+	function setActiveNav(e){
+		var currentNav = $(e.target).closest('li'),
+			navs = currentNav.siblings();
+		navs.removeClass('active');
+		currentNav.addClass('active');
+	}
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   #root
   def index
-    
+    @products = Product.limit(25)
+
   	respond_to do |format|
   	  format.html
     end
