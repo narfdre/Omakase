6671b27e744b90e408f7dfe6f147484f34f48856
Andre Kradolfer <narfdre@gmail.com>
2012-10-15 21:04:18 -0600

    getting prod to work

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,18 +3,18 @@ CarrierWave.configure do |config|
   config.storage              = :grid_fs
   config.grid_fs_access_url   = "/images"
 
-  if Rails.env.production?
-    config.grid_fs_database   = ENV['MONGOID_DATABASE']
-    config.grid_fs_host       = ENV['MONGOID_HOST']
-    config.grid_fs_port       = ENV['MONGOID_PORT']
-    config.grid_fs_username   = ENV['MONGOID_USERNAME']
-    config.grid_fs_password   = ENV['MONGOID_PASSWORD']
-  else
+  # if Rails.env.production?
+  #   config.grid_fs_database   = ENV['MONGOID_DATABASE']
+  #   config.grid_fs_host       = ENV['MONGOID_HOST']
+  #   config.grid_fs_port       = ENV['MONGOID_PORT']
+  #   config.grid_fs_username   = ENV['MONGOID_USERNAME']
+  #   config.grid_fs_password   = ENV['MONGOID_PASSWORD']
+  #else
     config.grid_fs_database     = Mongoid.database.name
     config.grid_fs_host         = Mongoid.database.connection.primary_pool.host
     config.grid_fs_port         = Mongoid.database.connection.primary_pool.port
     config.grid_fs_username     = Mongoid.database.connection.auths[0]['username']
     config.grid_fs_password     = Mongoid.database.connection.auths[0]['password']
-  end
+  #end
 
 end
